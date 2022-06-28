c5095858ccfd99da205ad9706132d9ed43841350
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-06-28 10:21:31 -0500

    Migrar proyecto a Vite.js

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LoginView from './security/login/login'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LoginView />
+  </React.StrictMode>
+)
