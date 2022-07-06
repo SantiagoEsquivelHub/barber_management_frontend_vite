72c2a4a38ede9e4c4ab294fbb74cdd7366cec083
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-07-06 00:13:41 -0500

    Ultimos cambios sidebar

--- src/containers/sidebar/Data/index.jsx
+++ src/containers/sidebar/Data/index.jsx
@@ -1,57 +1,34 @@
 export const Links = [
-    {
-      to: '/dashboard',
-      text: 'Dashboard',
-      svg: (
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M0 11.1111H8.88889V0H0V11.1111ZM0 20H8.88889V13.3333H0V20ZM11.1111 20H20V8.88889H11.1111V20ZM11.1111 0V6.66667H20V0H11.1111Z"
-            fill="white"
-          />
-        </svg>
-      ),
-    },
-    {
-      to: '/staff',
-      text: 'Staff',
-      svg: (
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M2 4H0V18C0 19.1 0.9 20 2 20H16V18H2V4ZM18 0H6C4.9 0 4 0.9 4 2V14C4 15.1 4.9 16 6 16H18C19.1 16 20 15.1 20 14V2C20 0.9 19.1 0 18 0ZM10 12.5V3.5L16 8L10 12.5Z"
-            fill="white"
-          />
-        </svg>
-      ),
-    },
-    {
-      to: '/users',
-      text: 'Users',
-      svg: (
-        <svg
-          width="20"
-          height="14"
-          viewBox="0 0 20 14"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M17 4H0V6H17V4ZM17 0H0V2H17V0ZM0 10H13V8H0V10ZM15 8V14L20 11L15 8Z"
-            fill="white"
-          />
-        </svg>
-      ),
-    }
-  ];
-  
+  {
+    to: '/dashboard',
+    text: 'Dashboard',
+    svg: (
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M0 11.1111H8.88889V0H0V11.1111ZM0 20H8.88889V13.3333H0V20ZM11.1111 20H20V8.88889H11.1111V20ZM11.1111 0V6.66667H20V0H11.1111Z"
+          fill="white"
+        />
+      </svg>
+    ),
+  },
+  {
+    to: '/staff',
+    text: 'Staff',
+    svg: (
+      <svg viewBox="0 0 24 24"  height="24" width="24" xmlns="http://www.w3.org/2000/svg" fill="white"><path fill="none" d="M0 0h24v24H0z"></path><path d="M19 3h-1V1h-2v2H8V1H6v2H5a2 2 0 00-2 2v14a2 2 0 002 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm6 12H6v-1c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1z"></path></svg>
+    ),
+  },
+  {
+    to: '/users',
+    text: 'Users',
+    svg: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+    ),
+  }
+];
