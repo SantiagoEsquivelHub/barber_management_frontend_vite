2aac72a04906be28fc9e4ae61ad91732622a38ba
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-07-12 16:41:27 -0500

    Creacion de usuario funcional, falta almacenar imagen enviada por el usuario y que todos los campos esten requeridos para que se pueda crear

--- src/modules/users/index.jsx
+++ src/modules/users/index.jsx
@@ -27,10 +27,12 @@ const UsersView = () => {
 
   const url = `http://${document.domain}:3001/usuarios/`;
   const urlRol = `http://${document.domain}:3001/roles/`;
+  const urlCrearUsuario = `http://${document.domain}:3001/usuario/`;
+
   let token = localStorage.getItem("token");
   let headers = new Headers();
   headers.append("Authorization", "Bearer " + token);
-
+  headers.append("Content-type", "application/json");
 
 
   const getUsers = async (e) => {
@@ -56,7 +58,7 @@ const UsersView = () => {
 
     const res = await fetch(urlRol, requestOptions);
     const data = await res.json();
-    console.log("roles", data);
+    //console.log("roles", data);
     setRol(data);
 
   }
@@ -76,11 +78,26 @@ const UsersView = () => {
     setVisible(true);
   };
 
-  const handleOk = () => {
+  const handleOk = async () => {
+
+    const requestOptions = {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify({
+        ...datos
+      }
+      )
+     
+    }
+
+    const res = await fetch(urlCrearUsuario, requestOptions);
+    console.log(res);
+
     setLoading(true);
     setTimeout(() => {
       setLoading(false);
       setVisible(false);
+      onReset();
     }, 3000);
   };
 
@@ -101,30 +118,63 @@ const UsersView = () => {
   };
 
   const normFile = (e) => {
-    console.log('Upload event:', e);
-
     if (Array.isArray(e)) {
-      return e;
+      return e.fileList;
     }
 
-    return e?.fileList;
+    return e.fileList;
   };
 
   const [datos, setDatos] = useState({
-    nombre_usuario: '',
-    documento_usuario: '',
-    telefono_usuario: '',
-    fecha_nacimiento_usuario: '',
-    correo_usuario: '',
-    estado_usuario: '',
-    url_img_usuario: '',
-    rol_usuario: ''
+    "nombre_usuario": '',
+    "documento_usuario": '',
+    "telefono_usuario": '',
+    "fecha_nacimiento_usuario": '',
+    "correo_usuario": '',
+    "estado_usuario": '',
+    "url_img_usuario": '',
+    "rol_usuario": ''
   })
 
+
   const handleInputChange = (e) => {
+
+
+      setDatos({
+        ...datos,
+        [e.target.name]: e.target.value
+      })
+    
+
+
+  }
+
+  const handleSelectChange = (value) => {
+
     setDatos({
       ...datos,
-      [e.target.name]: [e.target.value]
+      estado_usuario: '1',
+      rol_usuario: `${value}`
+    })
+
+  };
+
+  const props = {
+    name: 'file',
+    headers: {
+      authorization: 'authorization-text',
+    },
+  }
+
+  const getUrl = () => {
+
+    const fileInput = document.getElementById('url_img_usuario');
+
+    const selectedFile = fileInput.files[0];
+
+    setDatos({
+      ...datos,
+      [fileInput.id]: '/src/assets/images_users/' + selectedFile.name
     })
   }
 
@@ -157,12 +207,14 @@ const UsersView = () => {
 
           <Row className='d-flex align-items-center justify-content-center foto_perfil'>
             <Form.Item
-              name="upload"
+              name="url_img_usuario"
               label=""
               valuePropName="fileList"
               getValueFromEvent={normFile}
+              onChange={getUrl}
+              rules={[{ required: true }]}
             >
-              <Upload name="logo" action="/upload.do" listType="picture">
+              <Upload name="url_img_usuario" listType="picture" {...props} maxCount={1} id="url_img_usuario">
                 <Button icon={<UploadOutlined />}>Foto de perfil</Button>
               </Upload>
             </Form.Item>
@@ -172,17 +224,18 @@ const UsersView = () => {
             <div className='d-flex justify-content-center'>
               <Col span={12} className="m-3">
                 <Form.Item name="nombre_usuario" label="Nombre" rules={[{ required: true }]} className="d-flex flex-column">
-                  <Input type="text" onChange={handleInputChange} />
+                  <Input type="text" onChange={handleInputChange} name="nombre_usuario" />
                 </Form.Item>
                 <Form.Item name="fecha_nacimiento_usuario" label="Nacimiento" rules={[{ required: true }]} className="d-flex flex-column">
-                  <Input type="date" onChange={handleInputChange} />
+                  <Input type="date" onChange={handleInputChange} name="fecha_nacimiento_usuario" />
                 </Form.Item>
                 <Form.Item name="rol_usuario" label="Rol" rules={[{ required: true }]} className="d-flex flex-column">
                   <Select
                     defaultValue='Seleccione:'
                     placeholder=""
-                    onChange={handleInputChange}
+                    onChange={handleSelectChange}
                     allowClear
+                    name="rol_usuario"
                   >
                     {!rol ? 'Cargando...' :
 
@@ -197,13 +250,13 @@ const UsersView = () => {
               </Col>
               <Col span={12} className="m-3">
                 <Form.Item name="correo_usuario" label="Correo" rules={[{ required: true }]} className="d-flex flex-column">
-                  <Input type="text" onChange={handleInputChange} />
+                  <Input type="text" onChange={handleInputChange} name="correo_usuario" />
                 </Form.Item>
                 <Form.Item name="documento_usuario" label="Documento" rules={[{ required: true }]} className="d-flex flex-column">
-                  <Input type="text" onChange={handleInputChange} />
+                  <Input type="text" onChange={handleInputChange} name="documento_usuario" />
                 </Form.Item>
                 <Form.Item name="telefono_usuario" label="Teléfono" rules={[{ required: true }]} className="d-flex flex-column">
-                  <Input type="text" onChange={handleInputChange} />
+                  <Input type="text" onChange={handleInputChange} name="telefono_usuario" />
                 </Form.Item>
               </Col>
             </div>
